2202e8560ae2ee7d857e647e7e2dfd6fd1bc7f6d
anisa-matthews <anisa.matthews112gmail.com>
2019-12-08 22:38:27 -0500

    tryna see somethin pt 2

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var sceneL, sceneR;
 var plane, texture;
 
 let xZoom, yZoom, noiseStrength, simplex;
+var xZoom = 6;
+var yZoom = 18;
+var noiseStrength = 1.5;
+var simplex = new THREE.SimplexNoise();
 
 var worldWidth = 256, worldDepth = 256,
 	worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2;
@@ -33,8 +37,6 @@ function init() {
 	cameraR = new THREE.OrthographicCamera( 75, window.innerWidth / window.innerHeight, 0.1, 200 );
 	cameraR.position.z = 30;
 	cameraR.position.y = 30;
-
-	setupNoise();
 
 	scene1();
 	scene2();
@@ -114,15 +116,6 @@ function scene1(data){
 	sceneL.add( light );
 	var light = new THREE.AmbientLight( 0x222222 );
 	sceneL.add( light );
-}
-
-function setupNoise() {
-  // By zooming y more than x, we get the
-  // appearence of flying along a valley
-  xZoom = 6;
-  yZoom = 18;
-  noiseStrength = 1.5;
-  simplex = new THREE.SimplexNoise();
 }
 
 function scene2(){
@@ -266,61 +259,61 @@ function onWindowResize() {
 
 }
 
-function generateHeight( width, height ) {
-	var size = width * height, data = new Uint8Array( size ),
-		perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
-	for ( var j = 0; j < 4; j ++ ) {
-		for ( var i = 0; i < size; i ++ ) {
-			var x = i % width, y = ~ ~ ( i / width );
-			data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
-		}
-		quality *= 5;
-	}
-	return data;
-}
+// function generateHeight( width, height ) {
+// 	var size = width * height, data = new Uint8Array( size ),
+// 		perlin = new ImprovedNoise(), quality = 1, z = Math.random() * 100;
+// 	for ( var j = 0; j < 4; j ++ ) {
+// 		for ( var i = 0; i < size; i ++ ) {
+// 			var x = i % width, y = ~ ~ ( i / width );
+// 			data[ i ] += Math.abs( perlin.noise( x / quality, y / quality, z ) * quality * 1.75 );
+// 		}
+// 		quality *= 5;
+// 	}
+// 	return data;
+// }
 
-function generateTexture( data, width, height ) {
-	var canvas, canvasScaled, context, image, imageData, vector3, sun, shade;
-	vector3 = new THREE.Vector3( 0, 0, 0 );
-	sun = new THREE.Vector3( 1, 1, 1 );
-	sun.normalize();
-	canvas = document.createElement( 'canvas' );
-	canvas.width = width;
-	canvas.height = height;
-	context = canvas.getContext( '2d' );
-	context.fillStyle = '#000';
-	context.fillRect( 0, 0, width, height );
-	image = context.getImageData( 0, 0, canvas.width, canvas.height );
-	imageData = image.data;
-	for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
-		vector3.x = data[ j - 2 ] - data[ j + 2 ];
-		vector3.y = 2;
-		vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
-		vector3.normalize();
-		shade = vector3.dot( sun );
-		imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
-		imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-		imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
-	}
-	context.putImageData( image, 0, 0 );
-	// Scaled 4x
-	canvasScaled = document.createElement( 'canvas' );
-	canvasScaled.width = width * 4;
-	canvasScaled.height = height * 4;
-	context = canvasScaled.getContext( '2d' );
-	context.scale( 4, 4 );
-	context.drawImage( canvas, 0, 0 );
-	image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
-	imageData = image.data;
-	for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
-		var v = ~ ~ ( Math.random() * 5 );
-		imageData[ i ] += v;
-		imageData[ i + 1 ] += v;
-		imageData[ i + 2 ] += v;
-	}
-	context.putImageData( image, 0, 0 );
-	return canvasScaled;
-}
+// function generateTexture( data, width, height ) {
+// 	var canvas, canvasScaled, context, image, imageData, vector3, sun, shade;
+// 	vector3 = new THREE.Vector3( 0, 0, 0 );
+// 	sun = new THREE.Vector3( 1, 1, 1 );
+// 	sun.normalize();
+// 	canvas = document.createElement( 'canvas' );
+// 	canvas.width = width;
+// 	canvas.height = height;
+// 	context = canvas.getContext( '2d' );
+// 	context.fillStyle = '#000';
+// 	context.fillRect( 0, 0, width, height );
+// 	image = context.getImageData( 0, 0, canvas.width, canvas.height );
+// 	imageData = image.data;
+// 	for ( var i = 0, j = 0, l = imageData.length; i < l; i += 4, j ++ ) {
+// 		vector3.x = data[ j - 2 ] - data[ j + 2 ];
+// 		vector3.y = 2;
+// 		vector3.z = data[ j - width * 2 ] - data[ j + width * 2 ];
+// 		vector3.normalize();
+// 		shade = vector3.dot( sun );
+// 		imageData[ i ] = ( 96 + shade * 128 ) * ( 0.5 + data[ j ] * 0.007 );
+// 		imageData[ i + 1 ] = ( 32 + shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+// 		imageData[ i + 2 ] = ( shade * 96 ) * ( 0.5 + data[ j ] * 0.007 );
+// 	}
+// 	context.putImageData( image, 0, 0 );
+// 	// Scaled 4x
+// 	canvasScaled = document.createElement( 'canvas' );
+// 	canvasScaled.width = width * 4;
+// 	canvasScaled.height = height * 4;
+// 	context = canvasScaled.getContext( '2d' );
+// 	context.scale( 4, 4 );
+// 	context.drawImage( canvas, 0, 0 );
+// 	image = context.getImageData( 0, 0, canvasScaled.width, canvasScaled.height );
+// 	imageData = image.data;
+// 	for ( var i = 0, l = imageData.length; i < l; i += 4 ) {
+// 		var v = ~ ~ ( Math.random() * 5 );
+// 		imageData[ i ] += v;
+// 		imageData[ i + 1 ] += v;
+// 		imageData[ i + 2 ] += v;
+// 	}
+// 	context.putImageData( image, 0, 0 );
+// 	return canvasScaled;
+// }
 
 //ADD FUNCTIONALITY OF ROBOT//
 
