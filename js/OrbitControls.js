08fd916f1ad0f14c0b03e5ac29e60bb52c6462bf
anisa-matthews <anisa.matthews112gmail.com>
2019-12-08 20:53:09 -0500

    omgg

--- js/OrbitControls.js
+++ js/OrbitControls.js
@@ -13,7 +13,7 @@
 //    Zoom - middle mouse, or mousewheel / touch: two-finger spread or squish
 //    Pan - right mouse, or left mouse + ctrl/meta/shiftKey, or arrow keys / touch: two-finger move
 
-export default THREE.OrbitControls( object, domElement ) {
+THREE.OrbitControls = function( object, domElement ) {
 
 	this.object = object;
 
